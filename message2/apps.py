83fb43b446a83f07212207b9d40f301b774bb5a0
Yu Jing <jingyu@rd.netease.com>
2019-05-19 22:37:13 +0800

    django system

---
+++ message2/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Message2Config(AppConfig):
+    name = 'message2'
---
+++ message2/views.py
@@ -0,0 +1,33 @@
+# 添加message接口
+import sys
+from django.http import JsonResponse
+from jsonschema import ValidationError
+from message2.models import Message
+
+def add_message(request):
+    mid = request.POST.get('mid', '')
+    name = request.POST.get('name', '')
+    content = request.POST.get('content', '')
+    status = request.POST.get('status', '')
+    auther = request.POST.get('auther', '')
+
+    if mid == '' or name == '' or content == '' or auther == '':
+        return JsonResponse({'status': 10021, 'message': 'parameter error'})
+
+    result = Message.objects.filter(id=mid)
+    if result:
+        return JsonResponse({'status': 10022, 'message': 'event id already exists'})
+
+    result = Message.objects.filter(name=name)
+
+    if result:
+        return JsonResponse({'status': 10023,
+                             'message': 'event name already exists'})
+    try:
+        Message.objects.create(id=mid, name=name, content=content, auther=auther)
+    except:
+        return JsonResponse({'status': 10024, 'message': "参数格式有问题"})
+
+    return JsonResponse({'status':200,'message':'add message success'})
+
+
